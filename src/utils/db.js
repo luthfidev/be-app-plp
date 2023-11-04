6ce80f2aa5490a1c6ecb94a6092303e3a88be0f5
LW <lw@LWs-MacBook-Air.local>
2023-11-04 18:49:12 +0700

    fixing config db

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
   port: DB_PORT
 })
-connection.connect(function (error) {
+connection.getConnection(function (error) {
   if (error) {
     return connection.end()
   }
